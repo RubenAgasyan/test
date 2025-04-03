76c232cff28302a71af621e9f0e396fedb6073d2
RubenAgasyan <119342718+RubenAgasyan@users.noreply.github.com>
2025-04-03 18:38:21 +0300

    button to up

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+// Получаем кнопку стрелки
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Когда прокручиваем страницу, показываем или скрываем кнопку
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollToTopBtn.classList.add("show");
+    } else {
+        scrollToTopBtn.classList.remove("show");
+    }
+};
+
+// Функция для прокрутки страницы вверх
+scrollToTopBtn.addEventListener("click", function(event) {
+    event.preventDefault(); // Останавливаем стандартное действие ссылки
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
